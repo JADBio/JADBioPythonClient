fd0b15042811d1e169ea1a5e124bc1e50f367931
Stef <sfafalios@gnosisda.gr>
2021-03-31 14:54:30 +0300

    added timeout to helper.upload_dataset

--- examples/helper.py
+++ examples/helper.py
@@ -1,7 +1,7 @@
 import time
 
 # helper function for example
-def upload_dataset(client, pth, name, pid, has_snames=True):
+def upload_dataset(client, pth, name, pid, has_snames=True, timeout=10*60):
     """
     # Uploads (as is) and creates dataset synchronously from file path
     :param client: A client instance
@@ -9,6 +9,7 @@ def upload_dataset(client, pth, name, pid, has_snames=True):
     :param name: Name of the dataset to be created
     :param pid: Project id
     :param has_snames: Does the dataset have row headers?
+    :param timeout process will throw error if timeout is reached (sec)
     :return: created dataset id
     """
 
@@ -20,7 +21,10 @@ def upload_dataset(client, pth, name, pid, has_snames=True):
     tid = client.create_dataset(name, pid, file_id, fsz, has_sample_headers=has_snames)
     print("Uploaded dataset with tid: " + str(tid))
     status = client.get_task_status(tid)
+    start = time.time()
     while status['state'] != 'finished':
+        if time.time() - start > timeout:
+            raise TimeoutError("Timeout reached")
         time.sleep(3)
         status = client.get_task_status(tid)
     return status['datasetId']
