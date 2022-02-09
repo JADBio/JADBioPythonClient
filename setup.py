38aba82fa1d90e2052d2e2317bb6bfdf2dda2e41
borbudak-jadbio <borbudak@jadbio.com>
2022-02-09 09:41:17 +0100

    Bump version to 1.2.1

--- setup.py
+++ setup.py
@@ -3,4 +3,11 @@
 
 from setuptools import setup
 
-setup()
+setup(
+    # Description for pypi
+    long_description="""
+    JADBio's purpose-built AutoML platform provides leading-edge AI tools and automation capabilities enabling life-science professionals to build and deploy accurate and interpretable predictive models with speed and ease, even if they have no data science expertise.
+
+    This client provides the major JADBio functionality to python users using API calls. Requests are HTTP GET and POST only. POST requests are used for any kind of resource creation, mutation, or deletion. GET requests are read-only and idempotent.
+    """,
+    long_description_content_type='text/plain')
